1b8fb5b4d780f98035168cca36dd77be045782ea
Krystian Budulski <gibens4@gmail.com>
2023-04-12 00:28:10 +0200

    Update times.dart

--- styleguide/lib/src/tokens/times.dart
+++ styleguide/lib/src/tokens/times.dart
@@ -1,4 +1,4 @@
 abstract class AppTimes {
-  static const millis200 = Duration(milliseconds: 125);
+  static const millis200 = Duration(milliseconds: 200);
   static const millis400 = Duration(milliseconds: 400);
 }
